274fac64fafa801a23807e5e1830b9fdbd6bba3d
Jerónimo Pla <jpla@iti.upv.es>
2018-09-19 19:28:57 +0200

    Updates for config.json support

--- bin/nlocal-stamp.sh
+++ bin/nlocal-stamp.sh
@@ -1,2 +1,8 @@
-docker run --rm -it -e LSVERSION=latest -v /:/jost -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /eslap/component/server/scripts/_local-stamp-start.sh saasdk/local-stamp:latest
+#!/bin/bash
+CONFIG=$1
+VERSION=latest
+CONFIG_LINK=/tmp/.local-stamp.json
+[ -f $CONFIG_LINK ] && rm $CONFIG_LINK
+[ -f "$CONFIG" ] && shift && cp $CONFIG $CONFIG_LINK
+docker run --rm -it -e LSVERSION=$VERSION -v /:/jost -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /eslap/component/server/scripts/_local-stamp-start.sh saasdk/local-stamp:$VERSION $@
 
