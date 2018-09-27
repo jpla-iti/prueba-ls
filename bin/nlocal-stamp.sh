f972345a0eb39f59da7bbb6b9705384cc0fd6969
Jerónimo Pla <jpla@iti.upv.es>
2018-09-27 12:00:11 +0200

    Updated local-stamp launching mechanism

--- bin/nlocal-stamp.sh
+++ bin/nlocal-stamp.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-CONFIG=$1
-VERSION=latest
-CONFIG_LINK=/tmp/.local-stamp.json
-[ -f $CONFIG_LINK ] && rm $CONFIG_LINK
-[ -f "$CONFIG" ] && shift && cp $CONFIG $CONFIG_LINK
-docker run --rm -it -e LSVERSION=$VERSION -v /:/jost -v /var/run/docker.sock:/var/run/docker.sock --entrypoint /eslap/component/server/scripts/_local-stamp-start.sh saasdk/local-stamp:$VERSION $@
-
+docker run --rm -it --name local-stamp --net=host -v /var/run/docker.sock:/var/run/docker.sock -v /srv/local-stamp:/data saasdk/local-stamp:latest
